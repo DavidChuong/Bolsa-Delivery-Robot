e11ea32168d85f02121495c907011a2b1d3453ce
DavidChuong <33006538+DavidChuong@users.noreply.github.com>
2019-02-04 19:07:06 -0800

    Add files via upload

---
+++ PID_Control_for_BDR_V2.ino
@@ -0,0 +1,36 @@
+#include <PID_v1.h>
+
+#define LEFT_INPUT 0 //left encoder 
+#define RIGHT_INPUT 0 //right encoder 
+#define LEFT_OUTPUT_A 6 //left motor A
+#define LEFT_OUTPUT_B 8 //left motor B
+#define RIGHT_OUTPUT_A 5 //right motor A
+#define RIGHT_OUTPUT_B 10 //right motor B
+
+double LeftInput, RightInput, LeftOutput, RightOutput, error;
+double Kp = 0, Ki = 0, Kd = 0;
+
+void setup() {
+   LeftInput = analogRead(LEFT_INPUT); //read left encoder
+   RightInput = analogRead(RIGHT_INPUT); //read right encoder
+}
+
+void loop() {
+  //if left is greater than right, we reduce it to the speed of the right
+  if (LeftInput > RightInput) {
+    PID myPID(&LeftInput, &LeftOutput, &RightInput, Kp, Ki, Kd, DIRECT);
+    myPID.SetMode(AUTOMATIC); //intialize PID
+    myPID.Compute();
+    analogWrite(LEFT_OUTPUT_A, LeftOutput);
+    analogWrite(LEFT_OUTPUT_B, LeftOutput);
+  }
+
+  //if right is greater than left, we reduce it to the speed of the left
+  else if (LeftInput < RightInput) {
+    PID myPID(&RightInput, &RightOutput, &LeftInput, Kp, Ki, Kd, DIRECT);
+    myPID.SetMode(AUTOMATIC); //intialize PID
+    myPID.Compute();
+    analogWrite(RIGHT_OUTPUT_A, RightOutput);
+    analogWrite(RIGHT_OUTPUT_B, RightOutput);
+  }
+}
